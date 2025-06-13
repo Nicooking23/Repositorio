530dbf2565f6bda135a06d65b415ed39f1ceeb64
Nicooking23 <ni.jaramilloz@duocuc.cl>
2025-06-13 17:56:25 -0400

    Add files via upload

---
+++ operacionListaNumeros.py
@@ -0,0 +1,19 @@
+valores = [5, 2, 8, 1, 9, 3]
+#mostrando lista inicial
+print(valores)
+#ordeno lista menor a mayor 
+valores.sort()
+#imprime lista ordenada 
+print(valores)
+#invertir la lista 
+valores.reverse()
+#imprimir valores inversos 
+print(valores)
+#obtiene el valor minimo 
+minimo = min(valores)
+#imprime el valor minimo
+print(minimo)
+#obtengo el valor maximo 
+maximo = max(valores)
+#imprimo el valor maximo 
+print(maximo)
